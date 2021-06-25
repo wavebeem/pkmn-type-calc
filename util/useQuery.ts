f0a8bc598db9cea74e0735331ac951e99c515789
Brian Mock <brian@mockbrian.com>
2021-06-25 16:06:20 -0700

    ok

--- util/useQuery.ts
+++ util/useQuery.ts
@@ -2,9 +2,14 @@ import { useRouter } from "next/router";
 
 export function useQuery(key: string): string | undefined {
   const router = useRouter();
+  console.log(router);
+  // TODO: File Next.js issue? `query` starts off empty even though `asPath`
+  // properly reflects the query params on first load...
   const values = router.query[key];
   if (Array.isArray(values)) {
     return values[values.length - 1];
   }
   return values;
 }
+
+export const get;
