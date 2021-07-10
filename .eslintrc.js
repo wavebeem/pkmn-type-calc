99304cc2082f72637f6f0cb80ce2fb95fc176bf9
Brian Mock <brian@mockbrian.com>
2021-07-09 23:04:27 -0700

    fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,23 @@
 /* eslint-env node */
 module.exports = {
-  parser: "@typescript-eslint/parser",
   env: {
     browser: true,
   },
   plugins: ["@typescript-eslint"],
   extends: [
+    "preact",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "preact",
   ],
   rules: {
     // "react/prop-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
     // TODO: I would like to enable this rule eventually
     "@typescript-eslint/explicit-module-boundary-types": "off",
+    // TODO: This rule should ignore the JSX pragma... should file an issue
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { varsIgnorePattern: "^h$" },
+    ],
   },
 };
